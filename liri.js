aa8511b76b21a216852aaad0e37e4852e74c5b50
chrisperez626 <chrisperez626@gmail.com>
2018-04-10 13:10:19 -0400

    last touches to liri

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+// initail variables for retrieving files
 require("dotenv").config();
 
 var fs = require("fs");
@@ -14,23 +15,27 @@ var spotify = new Spotify(keys.spotify);
 
 var client = new Twitter(keys.twitter);
 
+//variables for commandline arguements
 var command = process.argv[2];
 
 var value = process.argv[3];
 
-if(command === "spotify-this-song"){
-	spotify.search({ type: 'track', query: value })
-  .then(function(response) {
-    console.log(response.tracks.items[0].artists[0].name);
-    console.log(response.tracks.items[0].name);
-    console.log(response.tracks.items[0].preview_url);
-    console.log(response.tracks.items[0].album.name);
-  }).catch(function(err) {
-    console.log(err);
-  });
-}
-if(command === "my-tweets"){
-	var params = {screen_name: 'Christian Perez'};
+//access spotify api
+function Music (){
+		spotify.search({ type: 'track', query: value })
+  		.then(function(response) {
+		    console.log(response.tracks.items[0].artists[0].name);
+		    console.log(response.tracks.items[0].name);
+		    console.log(response.tracks.items[0].preview_url);
+		    console.log(response.tracks.items[0].album.name);
+  		}).catch(function(err) {
+    		console.log(err);
+  		});
+	}
+
+//access twitter api
+function twitter(){	
+	var params = {screen_name: value};
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 	  if (!error) {
 	  	for(var i = 1; i < tweets.length; i++){
@@ -40,10 +45,11 @@ if(command === "my-tweets"){
 	  }
 	});
 }
-if(command === "movie-this"){
+
+//acess omdb api
+function movie() {
 	request("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
-  // If the request is successful (i.e. if the response status code is 200)
   		if (!error && response.statusCode === 200) {
 		  	console.log(JSON.parse(body).Title);
 		  	console.log(JSON.parse(body).Year);
@@ -57,20 +63,45 @@ if(command === "movie-this"){
   		}
 	});
 }
+
+//sets up paramaters for command
+if(command === "spotify-this-song"){
+	
+	return Music();
+	
+}
+if(command === "my-tweets"){
+	
+	return twitter();
+}
+if(command === "movie-this"){
+	
+	return movie();
+}
 if(command === "do-what-it-says"){
+	//reads random.txt file and commences command
 	fs.readFile("random.txt", "utf8", function(error, data){
 		var dataArr = data.split(",");
-		spotify.search({ type: 'track', query: dataArr[1] })
-		  .then(function(response) {
-		    console.log(response.tracks.items[0].artists[0].name);
-		    console.log(response.tracks.items[0].name);
-		    console.log(response.tracks.items[0].preview_url);
-		    console.log(response.tracks.items[0].album.name);
-		  }).catch(function(err) {
-		    console.log(err);
-		  });
-		
+		if(dataArr[0] === "spotify-this-song"){
+			value = dataArr[1];
+			return Music();
+		}
+		if(dataArr[0] === "movie-this"){
+			value = dataArr[1];
+			return movie();
+		}
+		if(dataArr[0] === "my-tweets"){
+			return twitter();
+		}
+		else{
+			console.log("check random.txt and input proper command")
+		}
 	})
+}
+//in case command is invalid
+else{
+	console.log("not an available command" + "\ntry either spotify-this-song"
+		, "\nmy-tweets", "\nmovie-this", "\ndo-what-it-says");
 }
 
 
